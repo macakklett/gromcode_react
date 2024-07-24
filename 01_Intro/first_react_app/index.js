a9de9986af6fa011bc148ee147f50f405fbc15c9
macakklett <macanvitalijj@gmail.com>
2024-07-24 15:14:47 +0300

    first lesson

---
+++ 01_Intro/first_react_app/index.js
@@ -0,0 +1,9 @@
+const rootElement = document.querySelector('#root');
+
+const el = React.createElement(
+  'div',
+  { className: 'greeting' },
+  'Hello, React'
+);
+
+ReactDOM.render(el, rootElement);
