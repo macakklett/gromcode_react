b4b0dcf7c068fb811c2d043295b352ca5dcf065d
macakklett <macanvitalijj@gmail.com>
2024-07-25 21:25:39 +0300

    fix

--- base_pack/task/src/Profile.jsx
+++ base_pack/task/src/Profile.jsx
@@ -10,7 +10,7 @@ const Profile = props => {
         {props.userData.firstName} {props.userData.lastName}
       </div>
       <div className="profile__birth">
-        Was born {formatDate(props.userData.birthDate)} in {props.userData.birthPlace}
+        `Was born ${formatDate(props.userData.birthDate)} in ${props.userData.birthPlace}`
       </div>
     </div>
   );
