4c1f63d2aba2fe5bd2aa2046e611c7be4deef3c9
macakklett <macanvitalijj@gmail.com>
2024-07-25 20:51:34 +0300

    ls3-4

--- base_pack/task/src/Avatar.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import 'avatar.scss';
-
-const Avatar = props => {
-  return <img className="avatar" src={props.avatarUrl} alt={props.name} />;
-};
-
-export default Avatar;
--- base_pack/task/src/index.jsx
+++ base_pack/task/src/index.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import './comment.scss';
 import App from './App.jsx';
-import Comment from './Comment.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Comment />, rootElement);
+ReactDOM.render(<App />, rootElement);
