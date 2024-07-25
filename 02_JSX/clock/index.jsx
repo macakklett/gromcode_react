099fb380f69c898dfec948538853286daaf2d7c6
macakklett <macanvitalijj@gmail.com>
2024-07-25 13:49:35 +0300

    task4

---
+++ 02_JSX/clock/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App.jsx';
+
+const rootElement = document.querySelector('#root');
+
+const showSeconds = (time) => {
+  const seconds = new Date(time).getSeconds();
+
+  const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
+  const textColor = seconds % 2 !== 0 ? '#fff' : '#000';
+
+  const styles = {
+    color: textColor,
+    backgroundColor,
+  };
+
+  const el = (
+    <>
+      <div className="seconds" style={styles}>
+        {seconds}
+      </div>
+    </>
+  );
+  ReactDOM.render(el, rootElement);
+};
+
+const intervalId = setInterval(() => {
+  showSeconds(new Date());
+}, 1000);
--- base_pack/task/src/App.jsx
+++ base_pack/task/src/App.jsx
@@ -2,27 +2,16 @@ import React from 'react';
 
 const App = () => {
   return (
-    <main className="page">
-      <form className="login-form">
-        <h1 className="form-title">Profile</h1>
-        <div className="form-control">
-          <label className="form-label" htmlFor="name">
-            Name
-          </label>
-          <input className="form-input" type="text" id="name" name="name" value="anonymous" />
+    <>
+      <h1 className="title">Todo List</h1>
+      <main className="todo-list">
+        <div className="actions">
+          <input className="task-input" type="text" />
+          <button className="btn create-task-btn">Create</button>
         </div>
-        <div className="form-control">
-          <label className="form-label" id="age" htmlFor="age">
-            Age
-          </label>
-          <input className="form-input" type="number" value={17} name="age" />
-          <span style={{ color: 'red', fontWeight: 700 }}>To young</span>
-        </div>
-        <button className="submit-button" type="submit">
-          Submit
-        </button>
-      </form>
-    </main>
+        <ul className="list"></ul>
+      </main>
+    </>
   );
 };
 
