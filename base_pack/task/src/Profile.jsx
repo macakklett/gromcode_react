e66fc7b9f7e576b56302f0f1aef588cfe03394ae
macakklett <macanvitalijj@gmail.com>
2024-07-25 21:30:38 +0300

    fix

--- base_pack/task/src/Profile.jsx
+++ base_pack/task/src/Profile.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import moment from 'moment';
 
-const Profile = props => {
+const Profile = ({ userData }) => {
   const formatDate = date => moment(date).format('DD MMM YY');
 
   return (
     <div className="profile">
       <div className="profile__name">
-        {props.userData.firstName} {props.userData.lastName}
+        {userData.firstName} {userData.lastName}
       </div>
       <div className="profile__birth">
-        `Was born ${formatDate(props.userData.birthDate)} in ${props.userData.birthPlace}`
+        {`Was born ${formatDate(userData.birthDate)} in ${userData.birthPlace}`}
       </div>
     </div>
   );
