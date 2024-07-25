4260b7ba814acdf03ca59bd0e6e6f841495b76bd
macakklett <macanvitalijj@gmail.com>
2024-07-25 10:53:21 +0300

    task5

---
+++ 01_Intro/todo-list/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const el = <h1 className="title">Todo List</h1>;
+
+root.render(el);
