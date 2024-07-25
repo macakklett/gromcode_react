67e5fefb502d03d065cd96edfd5884bade0b48f4
macakklett <macanvitalijj@gmail.com>
2024-07-25 13:36:03 +0300

    task3

--- base_pack/task/src/index.jsx
+++ base_pack/task/src/index.jsx
@@ -5,4 +5,27 @@ import App from './App.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App />, rootElement);
+const showSeconds = time => {
+  const seconds = new Date(time).getSeconds();
+
+  const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
+  const textColor = seconds % 2 !== 0 ? '#fff' : '#000';
+
+  const styles = {
+    color: textColor,
+    backgroundColor,
+  };
+
+  const el = (
+    <>
+      <div className="seconds" style={styles}>
+        {seconds}
+      </div>
+    </>
+  );
+  ReactDOM.render(el, rootElement);
+};
+
+const intervalId = setInterval(() => {
+  showSeconds(new Date());
+}, 1000);
