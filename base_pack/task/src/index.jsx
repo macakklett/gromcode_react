39cad3d05598cd2ad35179210114fd2268051596
macakklett <macanvitalijj@gmail.com>
2024-07-27 21:26:52 +0300

    ls5-3

--- base_pack/task/src/index.jsx
+++ base_pack/task/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import GoodButton from './GoodButton.jsx';
+import Counter from './Counter.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<GoodButton />, rootElement);
+ReactDOM.render(<Counter />, rootElement);
