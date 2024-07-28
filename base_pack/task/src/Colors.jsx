2dc1bc7e1f46a33a1c45490f7de5610cf4669c1f
macakklett <macanvitalijj@gmail.com>
2024-07-28 10:46:29 +0300

    ref

--- base_pack/task/src/Colors.jsx
+++ base_pack/task/src/Colors.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+const RED = '#f00';
+const GREEN = '#0f0';
+const BLUE = '#00f';
 
 class Colors extends Component {
   setBodyBackgroundColor = color => (document.body.style.backgroundColor = color);
@@ -8,18 +11,18 @@ class Colors extends Component {
       <div className="colors">
         <button
           className="colors__button"
-          style={{ backgroundColor: '#f00' }}
-          onClick={e => this.setBodyBackgroundColor(e.target.style.backgroundColor)}
+          style={{ backgroundColor: RED }}
+          onClick={() => this.setBodyBackgroundColor(RED)}
         ></button>
         <button
           className="colors__button"
-          style={{ backgroundColor: '#0f0' }}
-          onClick={e => this.setBodyBackgroundColor(e.target.style.backgroundColor)}
+          style={{ backgroundColor: GREEN }}
+          onClick={() => this.setBodyBackgroundColor(GREEN)}
         ></button>
         <button
           className="colors__button"
-          style={{ backgroundColor: '#00f' }}
-          onClick={e => this.setBodyBackgroundColor(e.target.style.backgroundColor)}
+          style={{ backgroundColor: BLUE }}
+          onClick={() => this.setBodyBackgroundColor(BLUE)}
         ></button>
       </div>
     );
