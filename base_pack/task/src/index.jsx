2bd5724d91c5c0fb2d56cfcdb48428222b779fbd
macakklett <macanvitalijj@gmail.com>
2024-07-28 11:08:16 +0300

    ls5-5

--- base_pack/task/src/index.jsx
+++ base_pack/task/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Colors from './Colors.jsx';
+import Toggler from './Toggler.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Colors />, rootElement);
+ReactDOM.render(<Toggler />, rootElement);
