b7bae8bebc57fefeef4b6ceb0e56fa582dd4e2f4
macakklett <macanvitalijj@gmail.com>
2024-07-28 12:02:08 +0300

    ls5-6

---
+++ base_pack/task/src/ColorPicker.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+const colorsArr = [
+  { rgb: 'rgb(255, 127, 80)', colorText: 'Coral' },
+  { rgb: 'rgb(0, 255, 255)', colorText: 'Aqua' },
+  { rgb: 'rgb(255, 228, 196)', colorText: 'Bisque' },
+];
+
+class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: '',
+    };
+  }
+
+  onMouseEnterHandler = e => {
+    const colorEl = colorsArr.filter(
+      el => el.rgb === window.getComputedStyle(e.target).backgroundColor,
+    );
+    this.setState({ color: colorEl[0].colorText });
+  };
+
+  onMouseLeaveHandler = e => {
+    this.setState({ color: '' });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="picker__title">{this.state.color}</div>
+        <div>
+          <button
+            className="picker__button picker__button_coral"
+            onMouseEnter={this.onMouseEnterHandler}
+            onMouseLeave={this.onMouseLeaveHandler}
+          ></button>
+          <button
+            className="picker__button picker__button_aqua"
+            onMouseEnter={this.onMouseEnterHandler}
+            onMouseLeave={this.onMouseLeaveHandler}
+          ></button>
+          <button
+            className="picker__button picker__button_bisque"
+            onMouseEnter={this.onMouseEnterHandler}
+            onMouseLeave={this.onMouseLeaveHandler}
+          ></button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ColorPicker;
--- base_pack/task/src/index.jsx
+++ base_pack/task/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Toggler from './Toggler.jsx';
+import ColorPicker from './ColorPicker.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Toggler />, rootElement);
+ReactDOM.render(<ColorPicker />, rootElement);
