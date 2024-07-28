8bc9336d967cf74d392836ba0c3b843b870a33e5
macakklett <macanvitalijj@gmail.com>
2024-07-28 17:52:29 +0300

    fix

--- base_pack/task/src/Mailbox.jsx
+++ base_pack/task/src/Mailbox.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Mailbox = ({ unReadMessages }) => {
   return (
-    <>
+    <div className="mailbox">
       <div className="mailbox__text">Mailbox</div>
       {unReadMessages.length > 0 && <div className="mailbox__count">{unReadMessages.length}</div>}
-    </>
+    </div>
   );
 };
 
