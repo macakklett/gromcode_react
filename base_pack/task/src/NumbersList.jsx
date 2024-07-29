5b6ad59eb3ce184d34456300e51964576f4e165d
macakklett <macanvitalijj@gmail.com>
2024-07-29 13:52:48 +0300

    ls6-2

--- base_pack/task/src/NumbersList.jsx
+++ base_pack/task/src/NumbersList.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import User from './User';
 
-const NumbersList = ({ numbers }) => {
-  return (
-    <ol>
-      {numbers.map((el, index) => (
-        <li key={index}>{el}</li>
-      ))}
-    </ol>
-  );
-};
+export class NumbersList extends Component {
+  state = {
+    sortingType: null,
+  };
+
+  onSetSorting = () =>
+    this.setState({ sortingType: this.state.sortingType === 'asc' ? 'desc' : 'asc' });
+
+  render() {
+    const sortedList = this.state.sortingType
+      ? this.props.users
+          .slice()
+          .sort((a, b) => (this.state.sortingType === 'asc' ? a.age - b.age : b.age - a.age))
+      : this.props.users;
+
+    return (
+      <div>
+        <button className="btn" onClick={this.onSetSorting}>
+          {this.state.sortingType || '-'}
+        </button>
+        <ul className="users">
+          {sortedList.map(user => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default NumbersList;
--- base_pack/task/src/index.jsx
+++ base_pack/task/src/index.jsx
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import NumbersList from './NumbersList';
 
+const users = [
+  {
+    id: 'id-0',
+    age: 21,
+    name: 'Bob',
+  },
+  {
+    id: 'id-1',
+    age: 17,
+    name: 'Tom',
+  },
+  {
+    id: 'id-2',
+    age: 18,
+    name: 'Tad',
+  },
+  {
+    id: 'id-3',
+    age: 45,
+    name: 'Sam',
+  },
+];
+
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<NumbersList numbers={[1, 5, 9, 4, 3]} />, rootElement);
+ReactDOM.render(<NumbersList users={users} />, rootElement);
