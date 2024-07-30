73559a3e8e238b2364da277e92701159867687f6
macakklett <macanvitalijj@gmail.com>
2024-07-30 16:34:46 +0300

    fix

--- base_pack/task/src/Life.jsx
+++ base_pack/task/src/Life.jsx
@@ -10,6 +10,18 @@ export class Life extends Component {
     console.log('componentDidMount: API calls, subscriptions');
   };
 
+  shouldComponentUpdate = (nextProps, nextState) => {
+    console.log('decide to render or not to render');
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    console.log('some updates based on new props');
+  };
+
+  componentWillUnmount = () => {
+    console.log('cleanup before DOM related to component will be removed');
+  };
+
   render() {
     console.log('return React element to build DOM');
     return <div>Life</div>;
