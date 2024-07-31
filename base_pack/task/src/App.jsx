9ec449cb965365272746c1897098718522595828
macakklett <macanvitalijj@gmail.com>
2024-07-31 15:20:23 +0300

    ls8-2

---
+++ base_pack/task/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Clock from './Clock.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggle: false,
+    };
+  }
+  render() {
+    return (
+      <>
+        <div>
+          <button onClick={() => this.setState({ isToggle: !this.state.isToggle })}>Toggle</button>
+        </div>
+        {this.state.isToggle && (
+          <>
+            <Clock location="London" offset={0} />
+            <Clock location="Kiev" offset={2} />
+            <Clock location="New York" offset={5} />
+          </>
+        )}
+      </>
+    );
+  }
+}
+
+export default App;
