7dcf298db97f88a0a0256742a1918c96365a5fad
macakklett <macanvitalijj@gmail.com>
2024-08-01 19:17:46 +0300

    add

---
+++ 09_Form/05_Tic_Tac_Toe/src/Square.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Square extends Component {
+  render() {
+    return (
+      <button className="square" onClick={this.props.onClick}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+export default Square;
---
+++ 09_Form/05_Tic_Tac_Toe/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './Game';
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Game />, rootElement);
