cdf2342d2a9b8b85693d9c6f1588c6a7d6539473
macakklett <macanvitalijj@gmail.com>
2024-08-02 13:29:46 +0300

    add

---
+++ 10_Lifting_state_up/03_Calculator_temperature/src/BoilingVerdict.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class BoilingVerdict extends Component {
+  render() {
+    if (this.props.celsius >= 100) {
+      return <p>The water would boil.</p>;
+    }
+    return <p>The water would not boil.</p>;
+  }
+}
+
+export default BoilingVerdict;
