90d86b9f462893c52a1ec1a8a0cd2032a5c4b38a
macakklett <macanvitalijj@gmail.com>
2024-08-06 17:45:03 +0300

    add

--- base_pack/task/src/index.jsx
+++ base_pack/task/src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.scss';
+import Dimensions from './Dimensions.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Dimensions />, rootElement);
