e1edc48cec62de5d82739d4d5d23ed95b59aa495
macakklett <macanvitalijj@gmail.com>
2024-08-14 16:44:53 +0300

    fix

--- base_pack/task/src/App.jsx
+++ base_pack/task/src/App.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import { UserDataContext } from './user-data-context';
 
 const App = () => {
   const [userData, setUserData] = useState({
-    name: 'Nikola Tesla',
-    avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+    name: 'User',
+    avatar_url: '',
   });
+
+  useEffect(() => {
+    setUserData({
+      name: 'Nikola Tesla',
+      avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+    });
+  }, []);
 
   return (
     <div className="page">
