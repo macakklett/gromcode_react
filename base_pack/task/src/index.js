ab8b0ebefe24039a345fa79ef137c4b0bd83be69
macakklett <macanvitalijj@gmail.com>
2024-08-15 21:01:47 +0300

    fix

--- base_pack/task/src/index.js
+++ base_pack/task/src/index.js
@@ -1,27 +1,18 @@
 import { store, decrement, increment, reset } from './store';
 import './index.scss';
 
-const counterActions = document.querySelector('.counter__actions');
+const decrementButton = document.querySelector('[data-action="decrement"]');
+const incrementButton = document.querySelector('[data-action="increment"]');
+const resetButton = document.querySelector('[data-action="reset"]');
 const counterResult = document.querySelector('.counter__result');
 
-const handleActions = e => {
-  switch (e.target.closest('.counter__button').dataset.action) {
-    case 'decrement':
-      store.dispatch(decrement());
-      return;
+const onDecrement = () => store.dispatch(decrement());
+const onIncrement = () => store.dispatch(increment());
+const onReset = () => store.dispatch(reset());
 
-    case 'increment':
-      store.dispatch(increment());
-      return;
-
-    case 'reset':
-      store.dispatch(reset());
-      return;
-
-    default:
-      return null;
-  }
-};
+decrementButton.addEventListener('click', onDecrement);
+incrementButton.addEventListener('click', onIncrement);
+resetButton.addEventListener('click', onReset);
 
 store.subscribe(() => {
   const state = store.getState();
@@ -29,5 +20,3 @@ store.subscribe(() => {
   const result = state.reduce((acc, el) => acc + Number(el), 0);
   counterResult.textContent = state.length > 0 ? `${history} = ${result}` : '';
 });
-
-counterActions.addEventListener('click', handleActions);
