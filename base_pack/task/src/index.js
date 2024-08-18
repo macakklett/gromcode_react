3c55731de58106d2e7f8b5ab637c133490ffb266
macakklett <macanvitalijj@gmail.com>
2024-08-18 21:31:19 +0300

    ls3-2

--- base_pack/task/src/index.js
+++ base_pack/task/src/index.js
@@ -1,4 +1,4 @@
-import { store, decrement, increment, reset } from './store';
+import { store, decrement, increment } from './store';
 import './index.scss';
 
 const decrementButton = document.querySelector('[data-action="decrement"]');
@@ -8,15 +8,11 @@ const counterResult = document.querySelector('.counter__result');
 
 const onDecrement = () => store.dispatch(decrement());
 const onIncrement = () => store.dispatch(increment());
-const onReset = () => store.dispatch(reset());
 
 decrementButton.addEventListener('click', onDecrement);
 incrementButton.addEventListener('click', onIncrement);
-resetButton.addEventListener('click', onReset);
 
 store.subscribe(() => {
   const state = store.getState();
-  const history = state.join('');
-  const result = state.reduce((acc, el) => acc + Number(el), 0);
-  counterResult.textContent = state.length > 0 ? `${history} = ${result}` : '';
+  counterResult.textContent = state;
 });
