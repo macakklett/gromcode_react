136da8c446a34264cac2525b2841ed7293af8692
macakklett <macanvitalijj@gmail.com>
2024-08-20 14:30:41 +0300

    fix

--- base_pack/task/src/users/Pagination.jsx
+++ base_pack/task/src/users/Pagination.jsx
@@ -8,11 +8,11 @@ const Pagination = ({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) =
   return (
     <div className="pagination">
       <button className="btn" disabled={isPrevPageAvailable} onClick={goPrev}>
-        ←
+        {!isPrevPageAvailable && '←'}
       </button>
       <span className="pagination__page">{currentPage + 1}</span>
       <button className="btn" disabled={isNextPageAvailable} onClick={goNext}>
-        →
+        {!isNextPageAvailable && '→'}
       </button>
     </div>
   );
