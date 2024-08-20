2547022804723ef3dd81dfbb256a4047f0e39235
macakklett <macanvitalijj@gmail.com>
2024-08-20 11:47:17 +0300

    ls4-2

--- base_pack/task/src/App.jsx
+++ base_pack/task/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import Counter from './Counter.jsx';
-import { store } from './store.js';
+import Users from './users/Users';
+import store from './store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Counter />
+      <Users />
     </Provider>
   );
 };
