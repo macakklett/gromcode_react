fee27973aa8df97e313387ce0ad94fab0b393547
macakklett <macanvitalijj@gmail.com>
2024-08-22 14:48:42 +0300

    add

---
+++ redux/05_Selectors/01_Users_Pagination/src/users/UsersList.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from './users.actions';
+import { usersListSelector, currentPageSelector } from './users.selectors';
+import Pagination from './Pagination';
+import User from './User';
+
+const UsersList = ({ usersList, currentPage, changeCurrentPage }) => {
+  const totalItems = usersList.length;
+  const itemsPerPage = 3;
+
+  const goPrev = () => changeCurrentPage(-1);
+  const goNext = () => changeCurrentPage(1);
+
+  return (
+    <div>
+      <Pagination
+        goPrev={goPrev}
+        goNext={goNext}
+        currentPage={currentPage}
+        totalItems={totalItems}
+        itemsPerPage={itemsPerPage}
+      />
+      <ul className="users">
+        {usersList.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage).map(user => (
+          <User key={user.id} name={user.name} age={user.age} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const mapState = state => {
+  return {
+    usersList: usersListSelector(state),
+    currentPage: currentPageSelector(state),
+  };
+};
+
+const mapDispatch = {
+  changeCurrentPage: actions.changeCurrentPage,
+};
+
+export default connect(mapState, mapDispatch)(UsersList);
