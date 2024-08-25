7c7323186b9ccecdb2b0f261980caee31fc8520f
macakklett <macanvitalijj@gmail.com>
2024-08-25 14:51:57 +0300

    add

--- base_pack/task/src/store.js
+++ base_pack/task/src/store.js
@@ -1,5 +1,44 @@
 import { createStore } from 'redux';
-import { usersReducer } from './users/users.reducer';
 
-const store = createStore(usersReducer);
-export default store;
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+const RESET = 'RESET/DECREMENT';
+
+export const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+export const reset = () => {
+  return {
+    type: RESET,
+  };
+};
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+
+    case DECREMENT:
+      return state - 1;
+
+    case RESET:
+      return 0;
+
+    default:
+      return state;
+  }
+};
+
+export const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
--- base_pack/task/src/users/users.actions.js
+++
@@ -1,6 +0,0 @@
-export const CHANGE_FILTER_INPUT = 'CHANGE_FILTER_INPUT';
-
-export const changeFilterText = value => ({
-  type: CHANGE_FILTER_INPUT,
-  payload: { value },
-});
