b45fdb69a8b3cd0d900fdc078fa5a647fc824231
macakklett <macanvitalijj@gmail.com>
2024-09-10 13:33:02 +0300

    lc-10.09

---
+++ base_pack/task/src/UserTable.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Pagination from './Pagination';
+import User from './User';
+
+export class UserTable extends Component {
+  itemsPerPage = 3;
+
+  state = {
+    currentPage: 1,
+  };
+
+  goPrev = () => this.setState({ currentPage: this.state.currentPage - 1 });
+  goNext = () => this.setState({ currentPage: this.state.currentPage + 1 });
+
+  render() {
+    const currentUserIndex = this.itemsPerPage * (this.state.currentPage - 1);
+    const currentList = this.props.users.slice(
+      currentUserIndex,
+      currentUserIndex + this.itemsPerPage,
+    );
+
+    return (
+      <div>
+        <Pagination
+          goPrev={this.goPrev}
+          goNext={this.goNext}
+          currentPage={this.state.currentPage}
+          totalItems={this.props.users.length}
+          itemsPerPage={this.itemsPerPage}
+        />
+        <ul className="users">
+          {currentList.map(user => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default UserTable;
--- base_pack/task/src/index.jsx
+++ base_pack/task/src/index.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import UserTable from './UserTable';
+
 import './index.scss';
-import Auth from './Auth';
+
+const users = [
+  {
+    id: 'id-0',
+    age: 21,
+    name: 'Bob',
+  },
+  {
+    id: 'id-1',
+    age: 17,
+    name: 'Tom',
+  },
+  {
+    id: 'id-2',
+    age: 18,
+    name: 'Tad',
+  },
+  {
+    id: 'id-3',
+    age: 45,
+    name: 'Sam',
+  },
+  {
+    id: 'id-4',
+    age: 17,
+    name: 'Tom',
+  },
+  {
+    id: 'id-5',
+    age: 18,
+    name: 'Tad',
+  },
+  {
+    id: 'id-6',
+    age: 45,
+    name: 'Sam',
+  },
+];
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Auth />, rootElement);
+ReactDOM.render(<UserTable users={users} />, rootElement);
